493247386a7110f052e9157e2b326662e65dda28
HacAtac <jhackworth00@outlook.com>
2022-12-11 17:19:58 -0600

    Initial Setup of Oauth auth server

---
+++ Oauth-authorization-server/src/main/java/com/hacatac/oauthserver/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hacatac.oauthserver.repository;
+
+
+import com.hacatac.oauthserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    User findByEmail(String email);
+}
---
+++ Oauth-authorization-server/src/main/java/com/hacatac/oauthserver/service/CustomUserDetailsService.java
@@ -0,0 +1,10 @@
+package com.hacatac.oauthserver.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService {
+}
