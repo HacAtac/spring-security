0e89ab4b0756018737a54eb523f32022abcc5945
HacAtac <jhackworth00@outlook.com>
2022-12-11 16:45:32 -0600

    Change password flow

--- spring-security-client/src/main/java/com/hacatac/security/client/controller/RegistrationController.java
+++ spring-security-client/src/main/java/com/hacatac/security/client/controller/RegistrationController.java
@@ -84,7 +84,16 @@ public class RegistrationController {
             return "Invalid Token";
         }
     }
-
+    @PostMapping("/changePassword")
+    public String changePassword(@RequestBody PasswordModel passwordModel) {
+        User user = userService.findUserByEmail(passwordModel.getEmail());
+        if (!userService.checkIfValidOldPassword(user, passwordModel.getOldPassword())) {
+            return "Invalid Old Password";
+        }
+        // Save New Password
+        userService.changePassword(user, passwordModel.getNewPassword());
+        return "Password Changed Successfully";
+    }
 
 
     private String passwordResetTokenMail(User user, String applicationUrl, String token) {
