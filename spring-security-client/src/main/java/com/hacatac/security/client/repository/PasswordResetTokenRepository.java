45320f052a207f0357bb249c4568f3f42cbe7f69
HacAtac <jhackworth00@outlook.com>
2022-12-11 16:37:06 -0600

    Rest password flow

---
+++ spring-security-client/src/main/java/com/hacatac/security/client/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.hacatac.security.client.repository;
+
+import com.hacatac.security.client.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+}
--- spring-security-client/src/main/java/com/hacatac/security/client/service/impl/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/hacatac/security/client/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.hacatac.security.client.service.impl;
 
+import com.hacatac.security.client.entity.PasswordResetToken;
 import com.hacatac.security.client.entity.User;
 import com.hacatac.security.client.entity.VerificationToken;
 import com.hacatac.security.client.model.UserModel;
+import com.hacatac.security.client.repository.PasswordResetTokenRepository;
 import com.hacatac.security.client.repository.UserRepository;
 import com.hacatac.security.client.repository.VerificationTokenRepository;
 import com.hacatac.security.client.service.UserService;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,6 +26,9 @@ public class UserServiceImpl implements UserService {
     private VerificationTokenRepository verificationTokenRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
 
     @Override
     public User registerUser(UserModel userModel) {
@@ -76,5 +82,51 @@ public class UserServiceImpl implements UserService {
         verificationTokenRepository.save(verificationToken);
 
         return verificationToken;
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void createPasswordResetTokenForUser(User user, String token) {
+        PasswordResetToken passwordResetToken =
+                new PasswordResetToken(user,token);
+        passwordResetTokenRepository.save(passwordResetToken);
+    }
+
+    @Override
+    public String validatePasswordResetToken(String token) {
+        PasswordResetToken passwordResetToken
+                = passwordResetTokenRepository.findByToken(token);
+
+        if (passwordResetToken == null) {
+            return "invalid";
+        }
+
+        User user = passwordResetToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if ((passwordResetToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
+            passwordResetTokenRepository.delete(passwordResetToken);
+            return "expired";
+        }
+
+        return "valid";
+
+    }
+
+    @Override
+    public Optional<User> getUserByPasswordResetToken(String token) {
+        return Optional.ofNullable(passwordResetTokenRepository.findByToken(token).getUser());
+    }
+
+    @Override
+    public void changePassword(User user, String newPassword) {
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
     }
 }
