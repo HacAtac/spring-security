bb0060d6e6b241bae469147c7c047ec000b7133e
HacAtac <jhackworth00@outlook.com>
2022-12-12 08:15:15 -0600

    Created CustomAuthenticationProvider and binded it to AuthenticationManagerBuilder

--- Oauth-authorization-server/src/main/java/com/hacatac/oauthserver/config/DefaultSecurityConfig.java
+++ Oauth-authorization-server/src/main/java/com/hacatac/oauthserver/config/DefaultSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.hacatac.oauthserver.config;
 
+import com.hacatac.oauthserver.service.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +22,12 @@ public class DefaultSecurityConfig {
         )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
     }
 
 }
